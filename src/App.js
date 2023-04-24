e9d93614f9fb010aeba121c0518b3686a5e1d925
Sardor <cr720051010@gmail.com>
2023-04-24 17:36:04 +0300

    router-dom lessons by pedroTech

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+    createBrowserRouter,
+    createRoutesFromElements,
+    Outlet,
+    Link,
+    Route,
+    RouterProvider
+} from "react-router-dom";
+import Home from "./Home";
+import Contact from "./Contact";
+import Data from "./Data";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const route = createBrowserRouter(
+        createRoutesFromElements(
+            <Route path="/" element={<Root/>}>
+                <Route path="/" index element={<Home/>}></Route>
+                <Route path="/contact" element={<Contact/>}></Route>
+                <Route path="/data" element={<Data/>}></Route>
+            </Route>
+        )
+    )
+
+    return (
+        <div className="App">
+            <RouterProvider router={route} />
+        </div>
+    );
 }
 
 export default App;
+
+
+const Root = () => {
+    return (
+        <>
+            <div>
+                <Link to="/">Home</Link>
+                <Link to="/data">Data</Link>
+                <Link to="/contact">Contact</Link>
+            </div>
+            <div>
+                <Outlet></Outlet>
+            </div>
+        </>
+    )
+}
